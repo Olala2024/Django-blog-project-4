183f147b89b3c6fb6b1034be32c476b97ca3b574
Olha Burtseva <olya.burtseva@gmail.com>
2024-11-21 21:35:23 +0000

    Fix files structure

--- codestar/urls.py
+++ codestar/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', my_blog, name='blog'),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
