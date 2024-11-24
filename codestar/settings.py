cf61c5d736521802e8fe884a11522c67a026f249
Olha Burtseva <olya.burtseva@gmail.com>
2024-11-24 05:31:48 +0000

    Add automation tests

--- codestar/settings.py
+++ codestar/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -95,6 +96,9 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net/",
